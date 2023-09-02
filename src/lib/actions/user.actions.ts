9c503bb9a8bbfc2f4021da1d74bb650cdf232ae5
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-09-02 14:33:36 +0530

    create user action

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,9 +1,42 @@
-import prisma from "../prisma";
+import prisma from '../prisma';
 
 export async function getUserById(id: string) {
   return await prisma.user.findUnique({
     where: {
       id,
+    },
+  });
+}
+
+export async function createUser({
+  id,
+  email,
+  name,
+  mobile,
+  enroll,
+  college,
+  branch,
+  year,
+}: {
+  id: string;
+  email: string;
+  name: string;
+  mobile: string;
+  enroll: string;
+  college: string;
+  branch: string;
+  year: number;
+}) {
+  return await prisma.user.create({
+    data: {
+      id,
+      email,
+      name,
+      mobile,
+      enroll,
+      college,
+      branch,
+      year,
     },
   });
 }
