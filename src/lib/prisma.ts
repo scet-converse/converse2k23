87e18ea67c791f96d41f88914bfbbbf341bdefae
Krishnakalani111 <Krishnakalani7@gmail.com>
2023-09-11 15:18:57 +0530

    avoiding multiple instances of prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const prisma = new PrismaClient();
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
