edad261f6b64e8b6e43c46f11af6c0e432d5df7d
Krishnakalani111 <Krishnakalani7@gmail.com>
2023-09-11 12:47:14 +0530

    marquee added

---
+++ src/components/Marquee.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+    <div className="marquee">
+  <div>
+    <span>++++ this is a running text aka marquee using html &amp; css + text length is limited to the width</span>
+    <span>++++ this is a running text aka marquee using html &amp; css + text length is limited to the width</span>
+  </div>
+</div>
+  )
+}
+
+export default Marquee
