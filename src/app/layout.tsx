ef205a8b20c41dcb3e6a72c14191d36e6404a7bf
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-09-12 19:14:20 +0530

    May solve fonts issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import localFont from 'next/font/local';
 
-const pixel = localFont({ src: '../../public/fonts/PixeloidSans.ttf' });
+const pixel = localFont({
+  src: '../../public/fonts/PixeloidSans.ttf',
+  preload: true,
+  display: 'block',
+});
 
 export const metadata: Metadata = {
   title: 'Converse',
