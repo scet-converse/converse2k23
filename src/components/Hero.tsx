f58b74f319e9a3a891ef64773772e430acebe3d0
jaimin <pkano455@gmail.com>
2023-09-14 19:42:08 +0530

    tv & marquee fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import Marquee from './Marquee';
 import Timer from './Timer';
+import Tv from './Tv';
 
 const Hero = () => {
   const [active, setActive] = React.useState(0);
@@ -111,18 +112,9 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="md:col-span-6 col-span-12 relative rounded-md md:h-full min-h-48 flex items-center justify-center px-8 py-6">
-          <div
-            className="absolute inset-0 bg-black rounded-md w-full h-full -z-10"
-            style={{
-              backgroundImage: `url('https://steamuserimages-a.akamaihd.net/ugc/802114790894531555/045F7A07B4272EF961134A63045A60BCB9A4CEC3/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')`,
-              backgroundSize: 'cover',
-              backdropFilter: 'blur(10px)', // Adjust the blur intensity as needed
-              opacity: '0.4', // Adjust the opacity as needed
-            }}
-          ></div>
-
+        <div className="md:col-span-6 col-span-12 relative rounded-md md:h-[130%] min-h-48 flex items-start justify-center ">
           <Timer startDate={FEST_START_AT} endDate={FEST_END_AT} />
+          <Tv/>
         </div>
       </div>
     </div>
--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,38 +1,17 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 type PropType = { sentences: string[] };
 
-const marqueeVariants = {
-  animate: {
-    x: ['100%', '-100%'],
-    transition: {
-      x: {
-        repeat: Infinity,
-        repeatType: 'loop',
-        duration: 10,
-        ease: 'linear',
-      },
-    },
-  },
-};
-
 const Marquee = ({ sentences }: PropType) => {
   return (
-    <div className="relative w-screen max-w-full h-auto overflow-hidden">
-      <motion.div
-        className="whitespace-nowrap flex items-center justify-start gap-4"
-        variants={marqueeVariants}
-        animate="animate"
-      >
-        {sentences &&
-          sentences.map((sentence, index) => (
-            <h1 className="md:text-lg" key={index}>
-              {sentence}
-            </h1>
-          ))}
-      </motion.div>
-    </div>
+    <div id="marquee" className='whitespace-nowrap overflow-x-auto overflow-y-hidden'>
+    {/* <h1 className='text-2xl inline-block mr-[30px]'>Welcome to the 2023 edition of Converse! <span className='px-[12px] py-2 bg-[#FE00D4] rounded-[15px] text-slate-950'>CONVERSE</span> !</h1> */}
+    <h1 className='text-2xl inline-block mr-[30px]'>WELCOME TO 2023 EDITION OF <span className='px-[12px] py-2 bg-[#FE00D4] rounded-[15px] text-slate-950'>CONVERSE</span> !</h1>
+    <h1 className='text-2xl inline-block mr-[30px]'>WELCOME TO 2023 EDITION OF <span className='px-[12px] py-2 bg-[#FE00D4] rounded-[15px] text-slate-950'>CONVERSE</span> !</h1>
+    <h1 className='text-2xl inline-block mr-[30px]'>WELCOME TO 2023 EDITION OF <span className='px-[12px] py-2 bg-[#FE00D4] rounded-[15px] text-slate-950'>CONVERSE</span> !</h1>
+    {/* <h1 className='text-2xl inline-block mr-[30px]'>HELLO STUDENTS WELCOME TO <span className='px-[12px] py-2 bg-[#FE00D4] rounded-[15px] text-slate-950'>CONVERSE</span> !</h1> */}
+
+</div>
   );
 };
 
