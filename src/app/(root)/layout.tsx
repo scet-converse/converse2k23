b24ecb3a3c89922075345295ec195a8a9d5cf361
Bhagya-Patel <bhagyapatel.it20@scet.ac.in>
2023-09-15 00:07:37 +0530

    Add not found page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,11 @@
+import Footer from '@/components/Footer';
 import React from 'react';
 
 function Layout({ children }: any) {
   return (
     <div className="px-4 md:px-[10%]">
       <div>{children}</div>
-      <p>Footer</p>
+      <Footer />
     </div>
   );
 }
