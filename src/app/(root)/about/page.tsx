4b5043c55f592c486db52217d34dd43de2bd76a4
jaimin <pkano455@gmail.com>
2023-09-17 19:09:23 +0530

    breadcrumb added in about page

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,10 +1,16 @@
 import { HOD_NAME } from '@/lib/constants';
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link'
 
 const page = () => {
   return (
-    <div className="flex flex-col items-center  w-full min-h-screen max-w-1200 mx-auto my-0 p-4 md:pt-20">
+    <div className="flex flex-col  w-full min-h-screen max-w-1200 mx-auto my-0 p-4 mt-8">
+      <h1 className="text-3xl mb-3 text-left">
+        <Link href="/">Home </Link> {'>'} About
+      </h1>
+      <p>Don't know about <span className='text-pink-600'>CONVERSE</span> ? , here you go.</p>
+
       <h1 className="mt-8 md:text-4xl text-xl text-center text-amber-400 font-extrabold ">🚀 Experience the Future at Converse 2K23 - <br className='hidden md:flex'/> Where Innovation Meets Inspiration! 🛸</h1>
       <div className="grid grid-cols-12 md:gap-9 gap-4 mt-4 items-center justify-center">
         <div className="col-span-12 h-full md:h-3/4 items-start justify-center my-4 flex overflow-hidden">
