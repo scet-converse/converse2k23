df4ef84f81c5df29d35253cf66ee2ed2ae4db07a
Rohitsonvane88 <rohitsonavare.it20@scet.ac.in>
2023-09-21 19:29:44 +0530

    Added sponsors data

--- src/lib/data/sponsors.ts
+++ src/lib/data/sponsors.ts
@@ -15,4 +15,25 @@ export default [
     description: `Our vision has led Rejoicehub Solutions to become a top IT company in India & USA for delivering various industry-led mobility solutions. The goal is to empower clients and businesses by creating new possibilities leveraging the technologies of today and tomorrow with the utmost quality, satisfaction, and transparency.`,
     link: 'https://rejoicehub.com/',
   },
+  {
+    id: '3',
+    logo: '/sponsors/farmhousehub.png',
+    name: 'FarmHouseHub',
+    description: `Modern farmhouse decor ideas and inspiration. We enjoy everything farmhouse, from old rustic style farmhouses to clean, modern style farmhouse decor. We use this blog to share our passion with our readers and help them find inspiration on how to decorate their homes.`,
+    link: 'https://farmhousehub.in/',
+  },
+  {
+    id: '4',
+    logo: '/sponsors/JAM_white.png',
+    name: 'Jay Ambe Med + Co',
+    description: `Modern farmhouse decor ideas and inspiration. We enjoy everything farmhouse, from old rustic style farmhouses to clean, modern style farmhouse decor. We use this blog to share our passion with our readers and help them find inspiration on how to decorate their homes.`,
+    link: 'https://g.co/kgs/ujBKKq',
+  },
+  {
+    id: '5',
+    logo: '/sponsors/aum.png',
+    name: 'AUM Insure & Secure',
+    description: `AUM INSURE & SECURE was founded in 2004 with the goal of providing world-class services to customers in order to help them achieve their financial independence with "Complete Peace of Mind". The firm structure has grown from one team member to a team of professionals today, and the same ethics and transparency run through the company and every team member.`,
+    link: 'https://www.aumispl.com/',
+  },
 ];
