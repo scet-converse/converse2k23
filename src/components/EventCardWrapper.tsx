8a03120fa54fd311606d13ae50432b2aebecbecc
Krishnakalani111 <Krishnakalani7@gmail.com>
2023-09-22 19:00:00 +0530

    added Animation on Scroll for event page

---
+++ src/components/EventCardWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const EventCardWrapper = ({ children }: any) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div data-aos="fade-up" data-aos-duration="1500">
+      {children}
+    </div>
+  );
+};
+
+export default EventCardWrapper;
