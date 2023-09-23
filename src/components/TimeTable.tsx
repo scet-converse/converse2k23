98a6e1c00d3411a3a91dd83c858c1d9833da4954
jaimin <pkano455@gmail.com>
2023-09-23 14:52:06 +0530

    theme chnged

--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TimeTable() {
   return (
     <div className="flex flex-col">
-      <h1 className="text-center text-2xl md:text-4xl text-[#9333EA] py-10 decoration-dotted underline">
+      <h1 className="text-center text-2xl md:text-4xl text-[#9333EA] py-10 ">
         ⏳ Events Scheduling ⌛{' '}
       </h1>
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
